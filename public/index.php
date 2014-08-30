e441366da07517f643763d7510ad3f3612c91f38
Gabor Molnar <gabormolnar92@gmail.com>
2014-08-30 22:20:54 +0200

    Paths corrections

--- public/index.php
+++ public/index.php
@@ -18,8 +18,7 @@
 |
 */
 
-//require __DIR__.'/../bootstrap/autoload.php';
-require __DIR__.'/../laravel/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,8 +32,7 @@ require __DIR__.'/../laravel/bootstrap/autoload.php';
 |
 */
 
-//$app = require_once __DIR__.'/../bootstrap/start.php';
-$app = require_once __DIR__.'/../laravel/bootstrap/start.php';
+$app = require_once __DIR__.'/../bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
